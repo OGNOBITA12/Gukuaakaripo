b64698cbf0302ecb11843ea81b0a9c29032cbfea
OGNOBITA12 <sumit07638@gmail.com>
2025-02-28 14:37:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GOKUxEDITION/NEWxSERVER",
+    "https://github.com/OGNOBITA12/Gukuaakaripo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
